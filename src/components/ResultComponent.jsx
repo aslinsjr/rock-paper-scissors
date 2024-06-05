f7540989bcb01f06e4ea6ad2179faabf309b5393
aslinsjr <aslinsjr@gmail.com>
2024-06-05 12:31:40 -0300

    Ajusto Responsivo

--- src/components/ResultComponent.jsx
+++ src/components/ResultComponent.jsx
@@ -1,26 +1,36 @@
 import './ResultComponent.css'
 
+import { useEffect, useState } from 'react'
+
 const ResultComponent = ({move, houseMove, win, handleReload}) => {
 
-  console.log(win)
+  const [delay, setDelay] = useState(false)
+
+  useEffect(() => {
+    const number2 = setTimeout(() => {
+      setDelay(true)
+    }, 1000)
+  }, [])
   
   return (
     <div className='result-container'>
-      <div className={`${move}-bg`}>
+      <div className={`${move}-bg`} id='user-move'>
         <img className={move} src={`icon-${move}.svg`} alt={`${move} Icon`} />
       </div>
-      {win ? 
+      {delay && <div>
+        {win ? 
       <div className='result-span'>
         <h2>You Win</h2>
         <button onClick={() => handleReload()}>Play again</button>
       </div> : 
       <div className='result-span'>
         <h2>Try again</h2>
-        <button onClick={() => handleReload()}>Play again</button>
-      </div>}
-      <div className={`${houseMove}-bg`}>
+        <button onClick={() => handleReload()}>Play</button>
+      </div>}</div>
+       }
+      <div className={`${houseMove}-bg`} id='house-move'>
         <img className={houseMove} src={`icon-${houseMove}.svg`} alt={`${houseMove} Icon`} />
-      </div> 
+      </div>
     </div>
   )
 }
